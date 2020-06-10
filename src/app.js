3a598df14ddbecb39b7ffbfd18a0b4137ba75d92
Tejas D S <tejasds1@in.ibm.com>
2020-06-10 21:31:35 +0530

    App setup to host

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = 8080
 
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../templates/views'))
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000')
 })
